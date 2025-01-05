6f7008a8662ec38e5699c9bf7ec7a0702f594f2e
Jordan <rhezajordan@gmail.com>
2025-01-05 14:54:57 -0500

    Add: Get, Post, Put, Delete AlquilerController

--- src/main/java/gm/alquilerVehiculos/Controller/AlquilerController.java
+++ src/main/java/gm/alquilerVehiculos/Controller/AlquilerController.java
@@ -1,18 +1,18 @@
 package gm.alquilerVehiculos.Controller;
 
-import gm.alquilerVehiculos.Dto.AlquilerDTO;
-import gm.alquilerVehiculos.Dto.ClienteDTO;
-import gm.alquilerVehiculos.Dto.EmpleadoDTO;
-import gm.alquilerVehiculos.Dto.VehiculoDTO;
+import gm.alquilerVehiculos.Dto.*;
+import gm.alquilerVehiculos.Exception.RecursoNoEncontradoException;
 import gm.alquilerVehiculos.models.Alquiler;
 import gm.alquilerVehiculos.models.Vehiculo;
 import gm.alquilerVehiculos.services.AlquilerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RestController
@@ -24,43 +24,116 @@ public class AlquilerController {
     //http://localhost:8080/api/alquilers
     @GetMapping("/alquilers")
     public List<AlquilerDTO> obtenerAlquileres() {
-        List<Alquiler> alquilers = alquilerService.listarAlquilers();
+        return alquilerService.listarAlquilers().stream()
+                .map(this::convertirAlquilerADTO)
+                .collect(Collectors.toList());
+    }
 
-        return alquilers.stream().map(alquiler -> {
-            Vehiculo vehiculo = alquiler.getVehiculo();
-            VehiculoDTO vehiculoDTO = new VehiculoDTO(
-                    vehiculo.getId(),
-                    vehiculo.getPlaca(),
-                    vehiculo.getTipo().getId(),
-                    vehiculo.getMarca().getId(),
-                    vehiculo.getEstado().getId()
-            );
+    @GetMapping("/alquilers/{id}")
+    public ResponseEntity<AlquilerDTO> obtenerAlquilerId(@PathVariable int id) {
+        Optional<Alquiler> alquilerOpt = Optional.ofNullable(alquilerService.buscarAlquilerPorId(id));
 
-            ClienteDTO clienteDTO = new ClienteDTO(
-                    alquiler.getId(), // Supongamos que `getId_cliente()` devuelve una entidad `Cliente`
-                    alquiler.getCliente().getNombre(),
-                    alquiler.getCliente().getEmail(),
-                    alquiler.getCliente().getTelefono()
-            );
+        if (alquilerOpt.isPresent()) {
+            AlquilerDTO alquilerDTO = convertirAlquilerADTO(alquilerOpt.get());
+            return ResponseEntity.ok(alquilerDTO);
+        } else {
+            throw new RecursoNoEncontradoException("No se encontro el id:" + id);
+        }
+    }
 
-            EmpleadoDTO empleadoDTO = new EmpleadoDTO(
-                    alquiler.getId(), // Supongamos que `getId_empleado()` devuelve una entidad `Empleado`
-                    alquiler.getEmpleado().getNombre(),
-                    alquiler.getEmpleado().getEmail(),
-                    alquiler.getEmpleado().getTelefono(),
-                    alquiler.getEmpleado().getUsuario()
-            );
+    @PutMapping("/alquilers/{id}")
+    public ResponseEntity<AlquilerDTO> actualizarAlquiler(@PathVariable int id, @RequestBody Alquiler alquilerResponse) {
+        Optional<Alquiler> alquilerOpt = Optional.ofNullable(alquilerService.buscarAlquilerPorId(id));
 
-            return new AlquilerDTO(
-                    alquiler.getId(),
-                    alquiler.getFecha_inicio(),
-                    alquiler.getFecha_fin(),
-                    alquiler.getFecha_entrega(),
-                    alquiler.getCosto(),
-                    vehiculoDTO,
-                    clienteDTO,
-                    empleadoDTO
-            );
-        }).collect(Collectors.toList());
+        if (alquilerOpt.isPresent()) {
+            Alquiler alquiler = alquilerOpt.get();
+            alquiler.setFecha_fin(alquilerResponse.getFecha_fin());
+            alquiler.setFecha_inicio(alquilerResponse.getFecha_entrega());
+            alquiler.setFecha_entrega(alquilerResponse.getFecha_entrega());
+            alquiler.setCosto(alquilerResponse.getCosto());
+
+            Alquiler actualizado = alquilerService.guardarAlquiler(alquiler);
+            AlquilerDTO alquilerDTO = convertirAlquilerADTO(actualizado);
+
+            return ResponseEntity.ok(alquilerDTO);
+        } else {
+            throw new RecursoNoEncontradoException("No se encontro el id:" + id);
+        }
+    }
+
+    @PostMapping("/alquiler")
+    public Alquiler agregarAlquiler(@RequestBody Alquiler alquiler) {
+        return this.alquilerService.guardarAlquiler(alquiler);
+    }
+
+    @DeleteMapping("/alquilers/{id}")
+    public ResponseEntity<Map<String, Boolean>> eliminarAlquiler(@PathVariable int id) {
+        Optional<Alquiler> alquilerOpt = Optional.ofNullable(alquilerService.buscarAlquilerPorId(id));
+
+        if(alquilerOpt.isPresent()) {
+            this.alquilerService.eliminarAlquilerPorId((alquilerOpt.get().getId()));
+            Map<String, Boolean> res = new HashMap<>();
+            res.put("Aquiler eliminado", Boolean.TRUE);
+            return ResponseEntity.ok(res);
+        }else {
+            throw new RecursoNoEncontradoException("No se encontro el id:" + id);
+        }
+    }
+
+    private AlquilerDTO convertirAlquilerADTO(Alquiler alquiler) {
+        VehiculoDTO vehiculoDTO = convertirVehiculoADTO(alquiler.getVehiculo());
+
+        ClienteDTO clienteDTO = new ClienteDTO(
+                alquiler.getCliente().getId(),
+                alquiler.getCliente().getNombre(),
+                alquiler.getCliente().getEmail(),
+                alquiler.getCliente().getTelefono()
+        );
+
+        EmpleadoDTO empleadoDTO = new EmpleadoDTO(
+                alquiler.getEmpleado().getId(),
+                alquiler.getEmpleado().getNombre(),
+                alquiler.getEmpleado().getEmail(),
+                alquiler.getEmpleado().getTelefono(),
+                alquiler.getEmpleado().getUsuario()
+        );
+
+        return new AlquilerDTO(
+                alquiler.getId(),
+                alquiler.getFecha_inicio(),
+                alquiler.getFecha_fin(),
+                alquiler.getFecha_entrega(),
+                alquiler.getCosto(),
+                vehiculoDTO,
+                clienteDTO,
+                empleadoDTO
+        );
+    }
+
+    private VehiculoDTO convertirVehiculoADTO(Vehiculo vehiculo) {
+        TipoDTO tipoDTO = new TipoDTO(
+                vehiculo.getTipo().getId(),
+                vehiculo.getTipo().getDescripcion(),
+                vehiculo.getTipo().getDescripcion_ampliada(),
+                vehiculo.getTipo().getCosto_alquiler()
+        );
+
+        MarcaDTO marcaDTO = new MarcaDTO(
+                vehiculo.getMarca().getId(),
+                vehiculo.getMarca().getNombre()
+        );
+
+        EstadoDTO estadoDTO = new EstadoDTO(
+                vehiculo.getEstado().getId(),
+                vehiculo.getEstado().getDescripcion()
+        );
+
+        return new VehiculoDTO(
+                vehiculo.getId(),
+                vehiculo.getPlaca(),
+                tipoDTO,
+                marcaDTO,
+                estadoDTO
+        );
     }
 }
